f3b70c4abf0dae5b52b281cff66dea5a93bc2dd0
selimolmaz <selimolmaz3.1415@gmail.com>
2023-01-28 18:29:04 +0300

    Threading Simple

--- main.py
+++ main.py
@@ -2,22 +2,19 @@ from models.Homan import Homan
 from modules.SayHallo import SayHallo
 from modules.PostRequest import Post
 from modules.GetRequst import getRequest
-from modules.Threading import thread_single, threads_multiple
+
 
 def main():
     h = Homan("selim", "OLMAZ")
-    function1 = h.eat()
-    function2 = h.move()
-    # thereading 
-    threads_multiple(function1, function2)
-    #thread_single(print(getRequest("http://api.plos.org/search?q=title:DNA")))
     
-    thread_single(Post("key1", "selam çukulatam"))
+    # thereading 
+    print(getRequest("http://api.plos.org/search?q=title:DNA"))
+    
+    print(Post("key1", "selam çukulatam"))
     
     #Post("say1", h.__str__())
     #print(getRequest("http://api.plos.org/search?q=title:DNA"))
     #bu asenkron çalışsın
-
 
 if __name__ == "__main__":
     main()
--- modules/Threading.py
+++ modules/Threading.py
@@ -1,23 +1,11 @@
 import threading
 import time
+
 # the function for doing two things in same time
-def threads_multiple(function1, function2):
-    # Create two threads
-    t1 = threading.Thread(target=function1)
-    t2 = threading.Thread(target=function2)
+def run_in_thread(func):
+    def wrapper(*args, **kwargs):
+        t = threading.Thread(target=func, args=args, kwargs=kwargs)
+        t.start()
+    return wrapper
 
-    # Start the threads
-    t1.start()
-    t2.start()
 
-    # Wait for both threads to finish
-    t1.join()
-    t2.join()
-
-    return print("Both threads finished.")
-
-def thread_single(function):
-    t = threading.Thread(target=function)
-    t.start()
-    t.join()
-    return print("single thread finished.")
